d058229acf5be6e3c3ba4098c1f1785a5184de77
cherrybrookcodes <254072711+cherrybrookcodes@users.noreply.github.com>
2026-02-02 19:22:01 +1100

    main icon working, text smoke implemented

--- bug-shrine/src/App.jsx
+++ bug-shrine/src/App.jsx
@@ -1,10 +1,14 @@
 import './styles/App.css'
 import Daycalc from './components/welcometext';
+import Mainicon from '/home/tom-bombadil/cherrybrookcodes/testing/bug-shrine/bug-shrine/src/assets/bug-main.png'
 
 function App() {
 return (
     <div className="App">
-        <Daycalc/>
+        <img src={Mainicon} className='icon'/>
+        <div className = "welcomecont">
+            <Daycalc/>
+        </div>
     </div>
 );
 }
--- bug-shrine/src/components/welcometext.jsx
+++ bug-shrine/src/components/welcometext.jsx
@@ -2,9 +2,9 @@ import '/home/tom-bombadil/cherrybrookcodes/testing/bug-shrine/bug-shrine/src/st
 
 const Daydisp = ({daydif}) => {
     return (
-            <div2 className="welcome">I have loved my Bug for {daydif} days! 
+            <div className="welcome">I have loved my Bug for {daydif} days! 
             <br></br>
-            So i made her this website.</div2>
+            So i made her this website.</div>
     )
 };
 
