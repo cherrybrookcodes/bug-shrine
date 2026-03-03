31eb69f1fb6b289c1622c29885a15b0ba7b11ab8
cherrybrookcodes <254072711+cherrybrookcodes@users.noreply.github.com>
2026-03-03 18:37:19 +1100

    finalized the page

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,31 @@
+import './styles/App.css';
+import Daycalc from './components/welcometext';
+import Mainicon from './assets/bug-main.png';
+import { Link } from "react-router";
+import Abouticon from './assets/bug-about.svg' ;
+import Homeicon from './assets/bug-home.svg' ;
+import Testingicon from './assets/bug-testing.svg' ;
+
 const About= () => {
   return (
-    <div>
-      <h1>Hello from profile page!</h1>
-      <p>So, how are you?</p>
+    <div className = "App">
+             <div className = "navbar">
+            <div><img src={Abouticon} className='navicon' /> </div>
+            <div><img src={Homeicon} className='navicon' /> </div>
+            <div><img src={Testingicon} className='navicon' /> </div>
+            <div className='welcome1'><Link to="About">About</Link></div>
+            <div className='welcome1'><Link to="/">Home</Link></div>
+            <div className='welcome1'>Testing</div>
+        </div>
+
+        <div className = 'welcomecont'>
+            <div className = "welcome">
+            I made this page to test out React Router, Publishing, using SVGs and using JSX. And also to tell my girlfriend i love her.
+            </div>
+        </div>
     </div>
   );
 };
+
 
 export default About;
--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import './styles/App.css';
 import Daycalc from './components/welcometext';
 import Mainicon from './assets/bug-main.png';
 import { Link } from "react-router";
+import Abouticon from './assets/bug-about.svg' ;
+import Homeicon from './assets/bug-home.svg' ;
+import Testingicon from './assets/bug-testing.svg' ;
 
 function App() {
 return (
     <div className="App">
     
         <div className = "navbar">
-            <div><Link to="About">About</Link></div>
-            <div><Link to="/">Home</Link></div>
-            <div className = "navelement">Testing</div>
+            <div><img src={Abouticon} className='navicon' /> </div>
+            <div><img src={Homeicon} className='navicon' /> </div>
+            <div><img src={Testingicon} className='navicon' /> </div>
+            <div className='welcome1'><Link to="About">About</Link></div>
+            <div className='welcome1'><Link to="/">Home</Link></div>
+            <div className='welcome1'>Testing</div>
         </div>
 
         <img src={Mainicon} className='icon'/>
